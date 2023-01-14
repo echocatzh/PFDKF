e3076d19f49e208cb971f6a11eabbd1e44e603d7
Shimin Zhang <ZSMLBY@outlook.com>
2023-01-15 03:01:19 +0800

    Update pfdkf.py

--- pfdkf.py
+++ pfdkf.py
@@ -1,18 +1,6 @@
 # Copyright 2022 Shimin Zhang <shmzhang@npu-aslp.org>
 # Version: 1.0
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 # reference paper: "STATE-SPACE ARCHITECTURE OF THE PARTITIONED-BLOCK-BASED
 # ACOUSTIC ECHO CONTROLLER"
 # =============================================================================
